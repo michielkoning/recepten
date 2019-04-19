6c23ad5f8b4e833df0fa7c66ef93465e92526dde
Michiel Koning <mail@michielkoning.nl>
2019-04-19 21:30:57 +0200

    clean css

--- app/src/App.vue
+++ app/src/App.vue
@@ -54,16 +54,4 @@ export default {
 
 <style lang="postcss">
 @import './styles/app.css';
-
-@font-face {
-  font-family: 'body';
-  src: url('/fonts/CrimsonPro-Roman-VF.ttf') format('truetype');
-  font-weight: 1 999;
-}
-
-@font-face {
-  font-family: 'header';
-  src: url('/fonts/MovementV.ttf') format('truetype');
-  font-weight: 1 999;
-}
 </style>
